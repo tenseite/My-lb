e1cb6007cc4a75d30ed7d8fa7a1777f3705a2972
tenseite <bogdahnnikonov@yandex.ru>
2025-06-26 09:45:38 +0300

    Add files via upload

---
+++ лаб 1 задание  4/лаб 1 задание  4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Прейскурант цен на велосипеды (можно расширить)
+        double[] bikePrices = { 15000, 22000, 18500, 30000, 12500, 27500 };
+
+        Console.WriteLine("Введите сумму денег у покупателя:");
+        double customerMoney = double.Parse(Console.ReadLine());
+
+        bool foundBike = false;
+
+        // Проверяем каждый велосипед в прейскуранте
+        foreach (double price in bikePrices)
+        {
+            if (price <= customerMoney)
+            {
+                foundBike = true;
+                Console.WriteLine($"Можно купить велосипед за {price} руб.");
+            }
+        }
+
+        if (!foundBike)
+        {
+            Console.WriteLine("Нет доступных велосипедов по вашей сумме");
+        }
+    }
+}
