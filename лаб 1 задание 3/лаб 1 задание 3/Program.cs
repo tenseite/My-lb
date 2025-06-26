87af7305dacfb59bc2a4df56d98c353559963d53
tenseite <bogdahnnikonov@yandex.ru>
2025-06-26 09:45:12 +0300

    Add files via upload

---
+++ лаб 1 задание 3/лаб 1 задание 3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите координату x:");
+        double x = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите координату y:");
+        double y = double.Parse(Console.ReadLine());
+
+        // Проверяем положение точки относительно границ
+        bool onBorder = (y == -x) || (y == x) || (y == 0);
+        bool insideArea = (y > -x) && (y < x) && (y > 0);
+
+        if (onBorder)
+        {
+            Console.WriteLine("На границе");
+        }
+        else if (insideArea)
+        {
+            Console.WriteLine("Да");
+        }
+        else
+        {
+            Console.WriteLine("Нет");
+        }
+    }
+}
