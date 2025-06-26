f73601725da8b5bc9126d2bf9e7559ba5062ca1c
tenseite <bogdahnnikonov@yandex.ru>
2025-06-26 09:46:31 +0300

    Add files via upload

---
+++ лаб 3/лаб 3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите начало диапазона (A):");
+        int A = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите конец диапазона (B):");
+        int B = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("\nЧетные числа, кратные 5 (while):");
+        int current = A;
+        while (current <= B)
+        {
+            if (current % 10 == 0)  // Числа, кратные 10 (четные и кратные 5)
+            {
+                Console.Write(current + " ");
+            }
+            current++;
+        }
+    }
+}
